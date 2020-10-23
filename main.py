0718055e3eb2819904397041cf24f65810911b3b
Igor Verizub <iverizub@raraavis.org>
2020-10-23 14:23:02 +0300

    Added base test

--- main.py
+++ main.py
@@ -35,3 +35,15 @@ def test_test5(page: Page):
     url = 'https://aws.amazon.com/'
     page.goto(url)
     assert page.url == url
+
+
+@pytest.mark.only_browser("chromium")
+def test_test6(page: Page):
+    url = 'https://amazon.com/'
+    page.goto(url)
+    page.click("input[aria-label=\"Search\"]")
+    page.fill("input[aria-label=\"Search\"]", "guitar")
+    with page.expect_navigation():
+        page.press("input[aria-label=\"Search\"]", "Enter")
+    page.click("//label/i")
+    assert 'Fender' in page.innerText('h2')
