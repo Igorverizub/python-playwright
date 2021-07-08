9324a226a17003541bb54af3587ff001bcbc4717
i.verizub <iverizub@raraavis.org>
2021-07-08 19:24:03 +0300

    Create new test and delete old tests

--- main.py
+++ main.py
@@ -1,49 +1,12 @@
 from playwright.sync_api import Page
-import pytest
+
+url = 'https://www.google.com/'
 
 
-@pytest.mark.only_browser("chromium")
-def test_test(page: Page):
-    url = 'https://aws.amazon.com/'
+def test_google_search_playwright(page: Page):
     page.goto(url)
-    assert page.url == url
-
-
-@pytest.mark.only_browser("chromium")
-def test_test2(page: Page):
-    url = 'https://aws.amazon.com/'
-    page.goto(url)
-    assert page.url == url
-
-
-@pytest.mark.only_browser("chromium")
-def test_test3(page: Page):
-    url = 'https://aws.amazon.com/'
-    page.goto(url)
-    assert page.url == url
-
-
-@pytest.mark.only_browser("chromium")
-def test_test4(page: Page):
-    url = 'https://aws.amazon.com/'
-    page.goto(url)
-    assert page.url == url
-
-
-@pytest.mark.only_browser("chromium")
-def test_test5(page: Page):
-    url = 'https://aws.amazon.com/'
-    page.goto(url)
-    assert page.url == url
-
-
-@pytest.mark.only_browser("chromium")
-def test_test6(page: Page):
-    url = 'https://amazon.com/'
-    page.goto(url)
-    page.click("input[aria-label=\"Search\"]")
-    page.fill("input[aria-label=\"Search\"]", "guitar")
+    page.click("[aria-label=\"Search\"]")
+    page.fill("[aria-label=\"Search\"]", "playwright")
     with page.expect_navigation():
-        page.press("input[aria-label=\"Search\"]", "Enter")
-    page.click("//label/i")
-    assert 'Fender' in page.innerText('h2')
+        page.click("text=Google Search")
+    assert page.is_visible("text=Playwright - Microsoft Edge Development | Microsoft Docs")
